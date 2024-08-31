9d1756aa1d770e044ea5c69f3e5d1242b8f7a7ec
Andrii Lebid <lebedav@gmail.com>
2024-08-30 23:16:48 -0300

    css edited, added information about me to index

--- www/index.php
+++ www/index.php
@@ -1,8 +1,32 @@
 <?php include 'inc/header.php'; ?>
 <?php include 'inc/nav.php'; ?>
 
-    <main>
-        <div class="photo"></div>
-    </main>
+
+				
+				<main>
+
+				<div class="notice">
+
+        <hr>
+				<div class = 'postimage'>
+					<div class='postitem'>
+								<img src="gallery/hotel_1.png" alt="Andrii Lebid">
+					</div>
+					
+					<div class='postitem'>
+						<div>
+							<h2>My short story</h2> 
+							<br>
+							<p>My name is Andrii Lebid, and I graduated from New Brunswick Community College with a Web and Mobile application development course in June 2024. Now that my studies have ended and I have finished a work-integrated internship in Advatek Systems, I am looking for job opportunities. I was happy to know that you have open positions because I believe I meet all the requirements. So, I would like to apply for this position. </p><br>
+							
+							<p>NBCC is not my first education; 20 years ago, I received an education at a university in Ukraine specializing in electronics production, after which I worked for 17 years as a technician. After arriving in Canada, I worked at Walmart for two years and am very familiar with customer service. The first time I came across web development was when I volunteered for the Harvest House charity organization, where I supported their corporate website, and I decided to go to college because I was very interested but needed more knowledge. During two years of college, I learned HTML5 and CSS3, Angular, JavaScript, C#, Java, PHP, databases MSSQL, server site development NODE, and ASP.NET. My knowledge in Web & Mobile Development, previous technical experience, and customer service experience is a unique combination that can help with many of your projects.</p><br>
+
+							<p>I am sure that my qualifications and previous experience will be beneficial to your company, and I would very much appreciate the opportunity to demonstrate my capabilities in person.</p>
+
+							</div>
+						</div>
+					</div>
+				</main>
+				
 
 <?php include 'inc/footer.php'; ?>
